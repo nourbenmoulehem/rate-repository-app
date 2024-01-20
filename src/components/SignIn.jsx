22899534806cc51a7479526168d2aa84adb87a34
Nour <nbenmoulehom@gmail.com>
2024-01-20 23:12:11 +0100

    Create Sign-in interface

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,60 @@
-import Text from './Text';
+import { Text, TextInput, Pressable, View, StyleSheet } from "react-native";
+import { Formik, useField } from "formik";
+
+import FormikTextInput from "./FormikTextInput";
+import theme from "../theme";
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+  },
+
+  button: {
+    backgroundColor: theme.colors.primary,
+    margin: 12,
+    padding: 12,
+    alignItems: "center",
+  },
+
+  buttonText: {
+    color: "white",
+  },
+});
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.container}>
+      <FormikTextInput name="username" placeholder="Username"   />
+      <FormikTextInput name="password" placeholder="password" secureTextEntry/>
+      <Pressable style={styles.button} onPress={onSubmit}>
+        <Text style={styles.buttonText}>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log("🚀 ~ onSubmit ~ values:", values);
+    const username = values.username;
+    const password = values.password;
+
+    if ( username && password ) {
+      console.log("🚀 ~ onSubmit ~ password:", password)
+      console.log("🚀 ~ onSubmit ~ username:", username)
+    }
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
