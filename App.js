7f122bee982a9942318d183204d4876ce727267e
Nour <nbenmoulehom@gmail.com>
2024-01-21 11:40:57 +0100

    test on env

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import Constants from 'expo-constants';
 
 import Main from './src/components/Main';
 
 const App = () => {
+
+  const env = Constants.expoConfig.extra.env;
+  console.log("🚀 ~ App ~ env:", env);  
   return (
     <>
       <NativeRouter>
